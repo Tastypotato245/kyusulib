7a18ecf9d45c122f9fc576b74ad00de5de2f0d7c
kyusulee <rbt2775@cau.ac.kr>
2024-01-15 16:45:32 +0900

    add: except: ft_free strarr free

--- ft_except/ft_free.c
+++ ft_except/ft_free.c
@@ -1,30 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_except.h                                        :+:      :+:    :+:   */
+/*   ft_free.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kyusulee <kyusulee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/15 13:37:36 by kyusulee          #+#    #+#             */
-/*   Updated: 2024/01/15 16:44:59 by kyusulee         ###   ########.fr       */
+/*   Created: 2024/01/15 16:43:40 by kyusulee          #+#    #+#             */
+/*   Updated: 2024/01/15 16:44:36 by kyusulee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_EXCEPT_H
-# define FT_EXCEPT_H
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <errno.h>
-# include "../libft/libft.h"
+#include <except.h>
 
-// ft_except.c
-void	exit_handler(int case_num, char *program_name, char *object);
-int		func_guard(int result);
-void	*null_guard(void *pointer);
+void	free_strarr(char **strarr)
+{
+	size_t	i;
 
-// ft_free.c
-void	free_strarr(char **strarr);
-
-#endif
+	if (strarr == NULL)
+		return ;
+	i = 0;
+	while (strarr[i])
+		free(strarr[i++]);
+	free(strarr);
+}
